f84f69c97b81fc68f12628a294ab231db094c205
Kerdo Arikas <arikas.kerdo@gmail.com>
2024-05-17 13:30:16 +0300

    Fix field types and duplicate id

--- register.php
+++ register.php
@@ -14,11 +14,11 @@
                     <h3>Rekistreeru</h3>
                     <div class="form-group">
                         <label for="username">Kasutajanimi</label>
-                        <input name="username" type="number" class="form-control" id="username">
+                        <input name="username" type="text" class="form-control" id="username">
                     </div>
                     <div class="form-group">
                         <label for="password">Parool</label>
-                        <input name="password" type="text" class="form-control" id="username">
+                        <input name="password" type="password" class="form-control" id="password">
                     </div>
                     <button type="submit" class="btn btn-dark mt-3 w-100">Registreeru</button>
                 </div>
